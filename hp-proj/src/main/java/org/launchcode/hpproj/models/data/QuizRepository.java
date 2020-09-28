e9642bac7e68f9ae2df8171a0cbf792ebab8aa08
April Webb <aprilewebb@gmail.com>
2020-09-28 13:45:56 -0400

    Added QuizRepo

---
+++ hp-proj/src/main/java/org/launchcode/hpproj/models/data/QuizRepository.java
@@ -0,0 +1,9 @@
+package org.launchcode.hpproj.models.data;
+
+import org.launchcode.hpproj.models.Quiz;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuizRepository extends CrudRepository<Quiz, Integer> {
+}
