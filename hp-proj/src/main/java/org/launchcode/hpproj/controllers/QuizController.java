a0df11fb64a422a586a495b97d89b65ca9404c88
April Webb <aprilewebb@gmail.com>
2020-10-14 21:32:20 -0400

    Created a json file to store quizzes

--- hp-proj/src/main/java/org/launchcode/hpproj/controllers/QuizController.java
+++ hp-proj/src/main/java/org/launchcode/hpproj/controllers/QuizController.java
@@ -1,5 +1,7 @@
 package org.launchcode.hpproj.controllers;
 
+import org.launchcode.hpproj.models.Question;
+import org.launchcode.hpproj.models.Quiz;
 import org.launchcode.hpproj.models.data.QuizRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,17 +15,18 @@ public class QuizController {
     @Autowired
     QuizRepository quizRepository;
 
-
+    Question[] triviaQuestions = {
+            new Question("What is Dumbledore's full name?", "b", "a, c, d")
+    };
 
 
     @GetMapping
     public String displayQuizzes(){
-        return "quizzes/index";
-    }
-
+        return"quizzes/index";
+        }
 
 //    @GetMapping
-//    public String takeQuiz(){
+//    public String triviaQuiz(){
 ////    Will need to iterate thru questions stored in array for each quiz
 //
 //
--- hp-proj/src/main/java/org/launchcode/hpproj/models/Question.java
+++ hp-proj/src/main/java/org/launchcode/hpproj/models/Question.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Question extends AbstractEntity{
@@ -13,15 +15,19 @@ public class Question extends AbstractEntity{
     @NotNull
     private String answer;
 
+    private String wrongAnswers;
+
+    private String letterValue;
+
     @ManyToOne
     @JoinColumn(name = "quiz_id")
     private Quiz quiz;
 //    How will I check the answer if it's a radio button and stored in array?
 
-
-    public Question(String prompt, String answer) {
+    public Question(String prompt, String answer, String wrongAnswers) {
         this.prompt = prompt;
         this.answer = answer;
+        this.wrongAnswers = wrongAnswers;
     }
 
     public Question() {
@@ -38,6 +44,10 @@ public class Question extends AbstractEntity{
 
     public String getAnswer() {
         return answer;
+    }
+
+    public String getWrongAnswers() {
+        return wrongAnswers;
     }
 
 }
